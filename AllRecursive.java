9581d4724647c793d20163bc9df14498c19a28df
Flowbie <adolan@hawaii.edu>
2023-04-18 16:49:01 -1000

    Comments commit

--- AllRecursive.java
+++ AllRecursive.java
@@ -1,28 +1,28 @@
-import java.io.*;
-import java.math.*;
+/******************************************************************************************************************
+* @Austin Dolan
+* ICS 211
+* AllRecursive.java
+* 4/18/2023
+******************************************************************************************************************/
 import java.util.Scanner;
 import java.util.InputMismatchException;
 
 public class AllRecursive {
    public static void main(String[] args) {
-      int choice = 0;
-      String greet = "Hello, I can perform 3 functions:"; 
-      String greetIntro = "1. Display a triangle of size n \n2. Print in decreasing order from n \n3. Perform an accumulation function of n \nAll by using recursion! \nWhich operation would you like to perform? (Press 1, 2, 3 and enter, or 0 to exit)";
+      int choice = 0;                                           // Choice used to loop menu until user selects 0
       Scanner scan = new Scanner(System.in);
-      System.out.println(greet);
-      System.out.println(greetIntro);
+      System.out.println("Hello, I can perform 3 functions: \n1. Display a triangle of size n \n2. Print in decreasing order from n \n3. Perform an accumulation function of n \nAll by using recursion! \nWhich operation would you like to perform? (Press 1, 2, 3 and enter, or 0 to exit)");
       try {
-         choice = scan.nextInt();
+         choice = scan.nextInt();                              // User selects 1, 2, 3, or 0
          menu(choice);
-      } catch (InputMismatchException as) {
+      } catch (InputMismatchException as) {                    // If user throws IME error set choice to -1 and rerun menu
          choice = -1;
          menu(choice);
       }
    }
 
-   public static void menu(int n) throws InputMismatchException{
+   public static void menu(int choice) throws InputMismatchException{
       Scanner scan = new Scanner(System.in);
-      int choice = n;
       if (choice != 1 && choice != 2 && choice != 3 && choice != 0) {
          System.out.println("Please press 1, 2, or 3 and enter or 0 to quit");
          try {
